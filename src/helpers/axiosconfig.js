0fc29237bd6d68b8d4d30f234919d722b08d3d81
Md Mohiuddin <mmd004322@gmail.com>
2021-05-06 14:31:48 +0600

    changed app structure

--- src/helpers/axiosconfig.js
+++ src/helpers/axiosconfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL="http://localhost:4000";
+const baseURL="https://mmmquickchat.herokuapp.com";
 
 const AxiosConfig=axios.create({
 	baseURL:baseURL
--- src/helpers/baseurl.js
+++ src/helpers/baseurl.js
@@ -1,3 +1,3 @@
-const baseURL='http://localhost:4000';
+const baseURL='https://mmmquickchat.herokuapp.com';
 
 export default baseURL;
